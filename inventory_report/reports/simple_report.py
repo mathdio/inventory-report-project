bc83d529659c1d3bf13c73fa97c68a119b28fde3
Matheus Diógenes <mathdiogenes@gmail.com>
2023-05-08 15:33:09 -0300

    feat: R02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,34 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, products_list):
+        manufacturing_dates = [
+            datetime.strptime(product["data_de_fabricacao"], "%Y-%m-%d").date()
+            for product in products_list
+        ]
+        oldest_date = min(manufacturing_dates)
+
+        expiration_dates = [
+            datetime.strptime(product["data_de_validade"], "%Y-%m-%d").date()
+            for product in products_list
+        ]
+        expiration_dates.sort(reverse=True)
+        today_date = datetime.today().date()
+        closest_date = None
+        for date in expiration_dates:
+            if date > today_date:
+                closest_date = date
+
+        company_names = [
+            product["nome_da_empresa"] for product in products_list
+        ]
+        company_most_common = Counter(company_names).most_common()[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_date}\n"
+            f"Data de validade mais próxima: {closest_date}\n"
+            f"Empresa com mais produtos: {company_most_common}"
+        )
