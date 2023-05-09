e4be2a9ce8966d0590ce4aeb87a8dfa87e6a7c20
Matheus Diógenes <mathdiogenes@gmail.com>
2023-05-09 14:30:16 -0300

    refactor: imports in main.py

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,8 +1,8 @@
 import sys
-from inventory_report.inventory.inventory_refactor import InventoryRefactor
-from inventory_report.importer.csv_importer import CsvImporter
-from inventory_report.importer.json_importer import JsonImporter
-from inventory_report.importer.xml_importer import XmlImporter
+from .inventory.inventory_refactor import InventoryRefactor
+from .importer.csv_importer import CsvImporter
+from .importer.json_importer import JsonImporter
+from .importer.xml_importer import XmlImporter
 
 
 def file_extension(path):
